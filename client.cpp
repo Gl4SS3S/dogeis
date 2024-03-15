fc357a6dce1589b8fcb5d7205d1c19daf3d09099
Gl4SS3S <jthurling78@gmail.com>
2024-03-15 07:11:07 +0200

    Added a request parser and the ability to read and write code from the incomming request

--- client.cpp
+++ client.cpp
@@ -1,3 +1,6 @@
+#include <cassert>
+#include <cerrno>
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -10,6 +13,8 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+const size_t k_max_msg = 4096;
+
 static void die(const char *msg) {
   int err = errno;
   fprintf(stderr, "[%d] %s\n", err, msg);
@@ -17,6 +22,69 @@ static void die(const char *msg) {
 }
 
 static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
+
+static int32_t read_full(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = read(fd, buf, n);
+    if (rv <= 0) {
+      return -1;
+    }
+
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+
+  return 0;
+}
+
+static int32_t write_all(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = write(fd, buf, n);
+    if (rv <= 0) {
+      return -1;
+    }
+
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+
+  return 0;
+}
+
+static int32_t query(int fd, const char *text) {
+  uint32_t len = (uint32_t)strlen(text);
+  if (len > k_max_msg) {
+    return -1;
+  }
+
+  char wbuf[4 + k_max_msg];
+  memcpy(wbuf, &len, 4);
+  memcpy(&wbuf[4], text, len);
+  if (int32_t err = write_all(fd, wbuf, 4 + len)) {
+    return err;
+  }
+
+  char rbuf[4 + k_max_msg + 1];
+  errno = 0;
+  int32_t err = read_full(fd, rbuf, 4);
+  if (err) {
+    if (err) {
+      if (errno == 0) {
+        msg("EOF");
+      } else {
+        msg("read () error");
+      }
+
+      return err;
+    }
+  }
+
+  memcpy(&len, rbuf, 4);
+  if (len > k_max_msg) {
+  }
+}
 
 static void do_something(int connfd) {
   char rbuf[64] = {};
