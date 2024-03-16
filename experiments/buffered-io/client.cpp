e047b8e96fa9aeecb5d629aede5dc20253bf7aab
Gl4SS3S <jthurling78@gmail.com>
2024-03-16 12:10:02 +0200

    Initial Experimenting Done

--- experiments/buffered-io/client.cpp
+++ experiments/buffered-io/client.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <cerrno>
+#include <cstddef>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
@@ -13,8 +14,13 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 3
+
 const size_t k_max_msg = 4096;
 
+int io_buffer = BUFFER_SIZE;
+
+char *io_content;
 static void die(const char *msg) {
   int err = errno;
   fprintf(stderr, "[%d] %s\n", err, msg);
@@ -98,6 +104,55 @@ static int32_t query(int fd, const char *text) {
   return 0;
 }
 
+static int32_t testing_io_buffer(int fd, const char *text) {
+  uint32_t len = (uint32_t)strlen(text);
+  if (len > k_max_msg) {
+    return -1;
+  }
+
+  printf("Is here\n");
+
+  char wbuf[4 + k_max_msg + 1];
+  memcpy(wbuf, &len, 4);
+  memcpy(&wbuf[4], text, len);
+  wbuf[4 + k_max_msg] = '\0';
+
+  /* if (int32_t err = write_all(fd, wbuf, 4 + len)) { */
+  /*   return err; */
+  /* } */
+  /**/
+  /* char rbuf[4 + k_max_msg + 1]; */
+  /* errno = 0; */
+  /* int32_t err = read_full(fd, rbuf, 4); */
+  /* if (err) { */
+  /*   if (err) { */
+  /*     if (errno == 0) { */
+  /*       msg("EOF"); */
+  /*     } else { */
+  /*       msg("read () error"); */
+  /*     } */
+  /**/
+  /*     return err; */
+  /*   } */
+  /* } */
+  /**/
+  /* memcpy(&len, rbuf, 4); */
+  /* if (len > k_max_msg) { */
+  /*   msg("too long"); */
+  /*   return -1; */
+  /* } */
+  /**/
+  /* err = read_full(fd, &rbuf[4], len); */
+  /* if (err) { */
+  /*   msg("read() error"); */
+  /*   return err; */
+  /* } */
+  /**/
+  /* rbuf[4 + len] = '\0'; */
+  /* printf("server says %s\n", &rbuf[4]); */
+  return 0;
+}
+
 static void do_something(int connfd) {
   char rbuf[64] = {};
   ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
@@ -116,6 +171,12 @@ int main() {
     die("socket()");
   }
 
+  io_content = (char *)malloc((BUFFER_SIZE) * sizeof(char[4 + k_max_msg + 1]));
+
+  if (io_content == NULL) {
+    die("Malloc Failed");
+  }
+
   struct sockaddr_in addr = {};
   addr.sin_family = AF_INET;
   addr.sin_port = ntohs(1234);                   // Port selection
@@ -125,6 +186,8 @@ int main() {
   if (rv) {
     die("connect");
   }
+
+  int32_t test = testing_io_buffer(fd, "testing");
 
   int32_t err = query(fd, "hello1");
   if (err) {
